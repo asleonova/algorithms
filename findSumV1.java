4081894da14b6f222a33cfde4e18ab338cf66185
Anna <asleonova.1@gmail.com>
2021-08-31 08:36:12 +0300

    findSum

---
+++ findSumV1.java
@@ -0,0 +1,12 @@
+public class findSumV1 {
+
+    public static int[] findSum(int[] arr) {
+        
+    } 
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int x = 5;
+        int[] res = findSum(arr);
+        System.out.println(res);
+    }
+}
